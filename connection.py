c68c10ceded3da7218bbda076f2054277951cd68
ShinySource <robert.henriquez.jara@gmail.com>
2024-05-08 01:52:25 -0400

    python-database: fix function

--- connection.py
+++ connection.py
@@ -56,43 +56,41 @@ class DAO(DataBase):
         except Exception as e:
             print("Error: ",str(e.args))
 
-    def verHistorial(self):
-        sql = "SELECT historial_contrato.id_historial_contrato, historial_contrato.id_contrato, historial_contrato.estado_anterior, historial_contrato.estado_nuevo, historial_contrato.fecha_cambio FROM historial_contrato"
+    def verHistorial(self, id):
+        sql = "SELECT historial_contrato.id_historial_contrato, historial_contrato.id_contrato, historial_contrato.estado_anterior, historial_contrato.estado_nuevo, historial_contrato.fecha_cambio FROM historial_contrato WHERE historial_contrato.id_historial_contrato = '"+str(id)+"'"
         try:
             self.cursor.execute(sql)
-            resultado = self.cursor.fetchall()
-            if len (resultado) == 0:
-                print("No hay Registros en el Historial")
+            resultado = self.cursor.fetchone()
+            if resultado != None:
+                print("    Id               : ",resultado[0])
+                print("    Id del Contrato  : ",resultado[1])
+                print("    Estado Anterior  : ",resultado[2])
+                print("    Estado Nuevo     : ",resultado[3])
+                print("    Apellido Materno : ",resultado[4])
+                print("    Fecha de Cambio  : ",resultado[5])
+                print("")
             else:
-                for obj in resultado:
-                    print("    Id               : ",obj[0])
-                    print("    Id del Contrato  : ",obj[1])
-                    print("    Estado Anterior  : ",obj[2])
-                    print("    Estado Nuevo     : ",obj[3])
-                    print("    Apellido Materno : ",obj[4])
-                    print("    Fecha de Cambio  : ",obj[5])
-                    print("")
+                print("No hay Registros en el Historial")
         except Exception as e:
             print("Error: ",str(e.args))
     
-    def verContrato(self):
-        sql = "SELECT contrato.id_contrato, contrato.id_usuario_trabajador, contrato.fecha_inicio, contrato.fecha_expiracion, contrato.tipo_contrato, contrato.contenido_contrato, contrato.estado, contrato.comentario FROM contrato"
+    def verContrato(self, id):
+        sql = "SELECT contrato.id_contrato, contrato.id_usuario_trabajador, contrato.fecha_inicio, contrato.fecha_expiracion, contrato.tipo_contrato, contrato.contenido_contrato, contrato.estado, contrato.comentario FROM contrato WHERE contrato.id_contrato = '"+str(id)+"'"
         try:
             self.cursor.execute(sql)
-            resultado = self.cursor.fetchall()
-            if len (resultado) == 0:
-                print("No hay Registros de Contratos")
+            resultado = self.cursor.fetchone()
+            if resultado != None:
+                print("    Id                      : ",resultado[0])
+                print("    Id del Usuario          : ",resultado[1])
+                print("    Fecha Inicio            : ",resultado[2])
+                print("    Fecha Expiracion        : ",resultado[3])
+                print("    Tipo de Contrato        : ",resultado[4])
+                print("    Contenido del Contrato  : ",resultado[5])
+                print("    Estado del Contrato     : ",resultado[6])
+                print("    Comentario del Contrato : ",resultado[7])
+                print("")  
             else:
-                for obj in resultado:
-                    print("    Id                      : ",obj[0])
-                    print("    Id del Usuario          : ",obj[1])
-                    print("    Fecha Inicio            : ",obj[2])
-                    print("    Fecha Expiracion        : ",obj[3])
-                    print("    Tipo de Contrato        : ",obj[4])
-                    print("    Contenido del Contrato  : ",obj[5])
-                    print("    Estado del Contrato     : ",obj[6])
-                    print("    Comentario del Contrato : ",obj[7])
-                    print("")
+                print("No hay Registros de Contratos")
         except Exception as e:
             print("Error: ",str(e.args))
 
@@ -106,8 +104,5 @@ class DAO(DataBase):
             msg = "Error: ",str(e.args)
         
         return msg
-
-
-a1 = DAO()
 
 
