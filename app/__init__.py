74c66d94feacd88f190ecaa6c3fa8a38afb5466a
Cyphound <bayrongomez.cyp@gmail.com>
2024-07-23 02:04:59 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{CLOUD_SQL_USER}:{CLOUD_SQL_PASSWORD}@127.0.0.1:3306/{CLOUD_SQL_DATABASE}'.format(**os.environ)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://{CLOUD_SQL_USER}:{CLOUD_SQL_PASSWORD}@127.0.0.1:3306/{CLOUD_SQL_DATABASE}'.format(**os.environ)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
