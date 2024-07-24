34c1dbbaf23c17e30a1cfd8c3f2807a10c6a6d50
Cyphound <bayrongomez.cyp@gmail.com>
2024-07-24 03:20:46 -0400

    Update contract_by_rut.py

--- app/routes/contract_by_rut.py
+++ app/routes/contract_by_rut.py
@@ -5,10 +5,10 @@ from .. import db
 
 contract_by_rut = Blueprint('contract_by_rut', __name__)
 
-@contract_by_rut.route('/get_contract_by_rut/<status>', methods=['GET'])
-def get_contracts(status):
+@contract_by_rut.route('/get_contract_by_rut/<rut>', methods=['GET'])
+def get_contract_by_rut(rut):
     try:
-        contracts = db.session.query(
+        contract = db.session.query(
             Contract,
             Contenido_Contrato.nombres,
             Contenido_Contrato.apellidos,
@@ -30,39 +30,38 @@ def get_contracts(status):
             Contenido_Contrato.asignacio_colacion,
             Contenido_Contrato.bono_asistencia
         ).join(Contenido_Contrato, Contenido_Contrato.id_contrato == Contract.id_contrato)\
-         .filter(Contract.estado == status).all()
+         .filter(Contenido_Contrato.rut == rut).first()
 
-        return jsonify([
-            {
-                'id': contract.id_contrato,
-                'estado': contract.estado,
-                'contrato': contract.contrato,
-                'comentario': contract.comentario,
-                'revision_gerente': contract.revision_gerente,
-                'revision_gerente_general': contract.revision_gerente_general,
-                'nombres': contenido_nombres,
-                'apellidos': contenido_apellidos,
-                'direccion': contenido_direccion,
-                'estado_civil': contenido_estado_civil,
-                'fecha_nacimiento': contenido_fecha_nacimiento,
-                'rut': contenido_rut,
-                'mail': contenido_mail,
-                'nacionalidad': contenido_nacionalidad,
-                'sistema_salud': contenido_sistema_salud,
-                'afp': contenido_afp,
-                'nombre_empleador': contenido_nombre_empleador,
-                'rut_empleador': contenido_rut_empleador,
-                'cargo': contenido_cargo,
-                'fecha_inicio': contenido_fecha_inicio,
-                'fecha_final': contenido_fecha_final,
-                'indefinido': contenido_indefinido,
-                'sueldo_base': contenido_sueldo_base,
-                'asignacio_colacion': contenido_asignacio_colacion,
-                'bono_asistencia': contenido_bono_asistencia
-            } for
-            contract, contenido_nombres, contenido_apellidos, contenido_direccion, contenido_estado_civil, contenido_fecha_nacimiento, contenido_rut, contenido_mail, contenido_nacionalidad, contenido_sistema_salud, contenido_afp, contenido_nombre_empleador, contenido_rut_empleador, contenido_cargo, contenido_fecha_inicio, contenido_fecha_final, contenido_indefinido, contenido_sueldo_base, contenido_asignacio_colacion, contenido_bono_asistencia
-            in contracts
-        ]), 200
+        if contract:
+            return jsonify({
+                'id': contract.Contract.id_contrato,
+                'estado': contract.Contract.estado,
+                'contrato': contract.Contract.contrato,
+                'comentario': contract.Contract.comentario,
+                'revision_gerente': contract.Contract.revision_gerente,
+                'revision_gerente_general': contract.Contract.revision_gerente_general,
+                'nombres': contract.nombres,
+                'apellidos': contract.apellidos,
+                'direccion': contract.direccion,
+                'estado_civil': contract.estado_civil,
+                'fecha_nacimiento': contract.fecha_nacimiento,
+                'rut': contract.rut,
+                'mail': contract.mail,
+                'nacionalidad': contract.nacionalidad,
+                'sistema_salud': contract.sistema_salud,
+                'afp': contract.afp,
+                'nombre_empleador': contract.nombre_empleador,
+                'rut_empleador': contract.rut_empleador,
+                'cargo': contract.cargo,
+                'fecha_inicio': contract.fecha_inicio,
+                'fecha_final': contract.fecha_final,
+                'indefinido': contract.indefinido,
+                'sueldo_base': contract.sueldo_base,
+                'asignacio_colacion': contract.asignacio_colacion,
+                'bono_asistencia': contract.bono_asistencia
+            }), 200
+        else:
+            return jsonify({"message": "No se encontró el contrato con ese RUT"}), 404
     except Exception as e:
         print(e)
         return jsonify({"error": str(e)}), 500
