454bc30fd531ec0c97a5158c9ceddbaa4acf1cee
Benjamin Morales <129797391+RyuDakai@users.noreply.github.com>
2024-07-24 05:17:20 -0400

    TEST CORREOS

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ def create_app():
     # Agregar la SECRET_KEY
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
-    # Configuración de Flask-Mail para Zoho
+    # Configuración de Flask-Mail para correos normales
     app.config['MAIL_SERVER'] = 'smtp.zoho.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
@@ -40,6 +40,17 @@ def create_app():
     db.init_app(app)
     mail.init_app(app)
 
+    # Configurar recovery_mail
+    recovery_mail_app = Flask(__name__)
+    recovery_mail_app.config['MAIL_SERVER'] = app.config['RECOVERY_MAIL_SERVER']
+    recovery_mail_app.config['MAIL_PORT'] = app.config['RECOVERY_MAIL_PORT']
+    recovery_mail_app.config['MAIL_USE_TLS'] = app.config['RECOVERY_MAIL_USE_TLS']
+    recovery_mail_app.config['MAIL_USE_SSL'] = app.config['RECOVERY_MAIL_USE_SSL']
+    recovery_mail_app.config['MAIL_USERNAME'] = app.config['RECOVERY_MAIL_USERNAME']
+    recovery_mail_app.config['MAIL_PASSWORD'] = app.config['RECOVERY_MAIL_PASSWORD']
+    recovery_mail_app.config['MAIL_DEFAULT_SENDER'] = app.config['RECOVERY_MAIL_DEFAULT_SENDER']
+    recovery_mail.init_app(recovery_mail_app)
+
     from .auth.login import auth_blueprint
     from .main.submit_form import main_blueprint
     from .main.submit_contract import contract_blueprint
@@ -57,7 +68,6 @@ def create_app():
     app.register_blueprint(contract_all_routes)
     app.register_blueprint(contract_by_rut)
     app.register_blueprint(recover_password_blueprint)
-
 
     @app.route('/')
     def index():
