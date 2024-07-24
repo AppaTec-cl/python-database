328e0897936f329fea1c09719db58f4865149cad
Benjamin Morales <129797391+RyuDakai@users.noreply.github.com>
2024-07-24 02:22:51 -0400

    Create recover_password.py

---
+++ app/main/recover_password.py
@@ -0,0 +1,60 @@
+from flask import Blueprint, request, jsonify
+from flask_mail import Message
+from itsdangerous import URLSafeTimedSerializer
+from ..models.user import User
+from .. import db, recovery_mail
+
+recover_password_blueprint = Blueprint('recover_password', __name__)
+
+serializer = URLSafeTimedSerializer('SECRET_KEY')  # Usa tu propia clave secreta
+
+@recover_password_blueprint.route('/request_reset', methods=['POST'])
+def request_reset():
+    data = request.get_json()
+    rut = data.get('rut')
+    user = User.query.filter_by(rut=rut).first()
+
+    if user:
+        token = serializer.dumps(user.rut, salt='password-reset-salt')
+        reset_url = f'http://yourfrontend.com/reset_password/{token}'
+
+        subject = "Recuperación de Contraseña"
+        body = (
+            f"Estimado {user.nombres},\n\n"
+            f"Ha solicitado restablecer su contraseña. Haga clic en el siguiente enlace para restablecer su contraseña:\n"
+            f"{reset_url}\n\n"
+            "Si no solicitó este cambio, puede ignorar este correo.\n\n"
+            "Saludos cordiales,\n"
+            "El Equipo de AppaTec"
+        )
+
+        msg = Message(subject, recipients=[user.mail])
+        msg.body = body
+        recovery_mail.send(msg)
+
+        return jsonify({"message": "Se ha enviado un correo electrónico con instrucciones para restablecer su contraseña"}), 200
+    else:
+        return jsonify({"error": "Usuario no encontrado"}), 404
+
+@recover_password_blueprint.route('/reset_password/<token>', methods=['POST'])
+def reset_password(token):
+    try:
+        rut = serializer.loads(token, salt='password-reset-salt', max_age=3600)  # Token válido por 1 hora
+    except:
+        return jsonify({"error": "Token inválido o expirado"}), 400
+
+    data = request.get_json()
+    new_password = data.get('new_password')
+    user = User.query.filter_by(rut=rut).first()
+
+    if user:
+        user.password = hash_password_bcrypt(new_password)
+        db.session.commit()
+        return jsonify({"message": "Contraseña actualizada exitosamente"}), 200
+    else:
+        return jsonify({"error": "Usuario no encontrado"}), 404
+
+def hash_password_bcrypt(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return base64.b64encode(hashed).decode('utf-8')
