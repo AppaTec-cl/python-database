9b639e13c74d7c5327b467bcd2632e291a2433a4
Benjamin Morales <129797391+RyuDakai@users.noreply.github.com>
2024-07-24 05:50:44 -0400

    back to commit

--- app/main/recover_password.py
+++ app/main/recover_password.py
@@ -2,10 +2,9 @@ from flask import Blueprint, request, jsonify
 from flask_mail import Message
 from itsdangerous import URLSafeTimedSerializer
 from ..models.user import User
-from .. import db, mail
+from .. import db, recovery_mail
 import bcrypt
 import base64
-import os
 
 recover_password_blueprint = Blueprint('recover_password', __name__)
 
@@ -32,8 +31,7 @@ def request_reset():
 
         msg = Message(subject, recipients=[user.mail])
         msg.body = body
-        msg.sender = os.environ.get('RECOVERY_EMAIL_USER')
-        mail.send(msg)
+        recovery_mail.send(msg)
 
         return jsonify({"message": "Se ha enviado un correo electrónico con instrucciones para restablecer su contraseña"}), 200
     else:
